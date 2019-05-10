6fb3c5e08370421495dafdb085d79ff51a67e99f
Asko Soukka <asko.soukka@iki.fi>
2019-05-10 10:10:42 +0300

    Upgrade configuration

--- setup.nix
+++ setup.nix
@@ -1,10 +1,10 @@
 { pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/aade6ded7969ff2173a9eda4cd2f11ef83ca2768.tar.gz";
-    sha256 = "1svfmxzznyph9kmg0zjkjwpf9373s9b980ggw4hn1pgni8iw6q48";
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/7bb74e653654dbf9206e751574b5132b15f46bb5.tar.gz";
+    sha256 = "1dbdy4f58yqz4l67n032184rx7ci94hx3wl52c8h2bg06awkzq87";
   }) {}
  , setup ? import (fetchTarball {
-    url = "https://github.com/datakurre/setup.nix/archive/08a51f7fc2cad7f22d50ba36d81529e7e4fec30c.tar.gz";
-    sha256 = "15gzkxpwmjfhvcsr6w2nhaz0jlwnl1ary8l5lz6j6fkmg3mwb6db";
+    url = "https://github.com/datakurre/setup.nix/archive/b453d65d700bf1e709538c03f5c67c3a01f6d406.tar.gz";
+    sha256 = "07qavdvv4qxs3fcvb6dwlyh23bvc0dgwx4pw339rsdv21zg60cv2";
   })
 #, setup ? import ../setup.nix
 , python ? "python3"
@@ -14,20 +14,17 @@
 
 let overrides = self: super: {
 
+  # TODO: why tests failed with Plone 4.3 versions...
   "funcsigs" = super."funcsigs".overridePythonAttrs(old: {
     doCheck = false;
   });
 
+  # TODO: why tests failed with Plone 4.3 versions...
   "mock" = super."mock".overridePythonAttrs(old: {
     doCheck = false;
   });
 
-  "zope.testing" = super."zope.testing".overridePythonAttrs(old: {
-    postInstall = ''
-      rm -f $out/bin/zope-testrunner
-    '';
-  });
-
+  # Should be fixed with new plone.dexterity release
   "plone.dexterity" = super."plone.dexterity".overridePythonAttrs(old: {
     patches = if old.name != "plone.dexterity-2.2.8" then [ ./plone.dexterity.102.patch ] else [];
   });
@@ -42,34 +39,17 @@ let overrides = self: super: {
     '';
   });
 
-  # Will be fixed by updating Products.CMFActionIcons
-  "Products.CMFActionIcons" = super."Products.CMFActionIcons".overridePythonAttrs(old: {
-    nativeBuildInputs = [ self."eggtestinfo" ];
-  });
-
-  # Will be fixed by updating Products.CMFCore
-  "Products.CMFCore" = super."Products.CMFCore".overridePythonAttrs(old: {
-    nativeBuildInputs = [ self."eggtestinfo" ];
-  });
-
-  # Will be fixed by updating jsonschema
+  # should be fixed by updating jsonschema
   "jsonschema" = super."jsonschema".overridePythonAttrs(old: {
     nativeBuildInputs = [ self."vcversioner" ];
   });
 
-  # Will be fixed by updating nixpkgs
+  # should be fixed by updating nixpkgs
   "testfixtures" = super."testfixtures".overridePythonAttrs(old: {
     patches = [];
   });
 
-  # https://github.com/zopefoundation/z3c.autoinclude/archive/pip.tar.gz
-  "z3c.autoinclude" = super."z3c.autoinclude".overridePythonAttrs (old: {
-    src = fetchTarball {
-      url = "https://github.com/zopefoundation/z3c.autoinclude/archive/8f8c603024979a44b95a3fd104fff02cdb208da1.tar.gz";
-      sha256 = "1mf11ivnyjdfmc2vdd01akqwqiss0q8ax624glxrzk8qx46spqqi";
-    };
-  });
-
+  # fix zc.buildout to generate scripts with nix wrapped python env
   "zc.buildout" = pythonPackages.zc_buildout_nix.overridePythonAttrs (old: {
     name = super."zc.buildout".name;
     src = super."zc.buildout".src;
